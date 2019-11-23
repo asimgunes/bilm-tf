cb4504bb8de0be46adc0a777e1af4ef22fab8a5d
Asım Güneş <asim@Asms-MacBook-Pro.local>
2019-11-24 02:10:20 +0300

    train_elmo parameters

--- bin/train_elmo.py
+++ bin/train_elmo.py
@@ -9,14 +9,16 @@ from bilm.data import BidirectionalLMDataset
 
 def main(args):
     # load the vocab
-    vocab = load_vocab(args.vocab_file, 50)
+    vocab_file = os.path.join(args.folder, 'vocabulary.txt')
+    vocab = load_vocab(vocab_file, 50)
 
     # define the options
     batch_size = 128  # batch size for each GPU
-    n_gpus = 3
+    n_gpus = args.gpu
 
     # number of tokens in training data (this for 1B Word Benchmark)
-    n_train_tokens = 768648884
+    #n_train_tokens = 768648884
+    n_train_tokens = args.tokens
 
     options = {
      'bidirectional': True,
@@ -46,7 +48,7 @@ def main(args):
     
      'all_clip_norm_val': 10.0,
     
-     'n_epochs': 10,
+     'n_epochs': args.epoch,
      'n_train_tokens': n_train_tokens,
      'batch_size': batch_size,
      'n_tokens_vocab': vocab.size,
@@ -54,20 +56,24 @@ def main(args):
      'n_negative_samples_batch': 8192,
     }
 
-    prefix = args.train_prefix
-    data = BidirectionalLMDataset(prefix, vocab, test=False,
-                                      shuffle_on_load=True)
+    prefix = os.path.join(args.folder, 'corpus', '*')
+    data = BidirectionalLMDataset(prefix, vocab, test=False, shuffle_on_load=True)
 
-    tf_save_dir = args.save_dir
-    tf_log_dir = args.save_dir
+    tf_save_dir = os.path.join(args.folder, args.checkpoint)
+    tf_log_dir = tf_save_dir
     train(options, data, n_gpus, tf_save_dir, tf_log_dir)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--save_dir', help='Location of checkpoint files')
-    parser.add_argument('--vocab_file', help='Vocabulary file')
-    parser.add_argument('--train_prefix', help='Prefix for train files')
+    #parser.add_argument('--save_dir', help='Location of checkpoint files')
+    #parser.add_argument('--vocab_file', help='Vocabulary file')
+    #parser.add_argument('--train_prefix', help='Prefix for train files')
+    parser.add_argument('folder', help='Training folder for biLM')
+    parser.add_argument('--tokens', help='Token count to train', type=int)
+    parser.add_argument('--checkpoint', help='Checkpoint folder for training data', default='checkpoint')
+    parser.add_argument('--gpu', help='Number of GPU to use in the training', type=int, default=1)
+    parser.add_argument('--epoch', help='Number epoch to traing', type=int, default=10)
 
     args = parser.parse_args()
     main(args)
